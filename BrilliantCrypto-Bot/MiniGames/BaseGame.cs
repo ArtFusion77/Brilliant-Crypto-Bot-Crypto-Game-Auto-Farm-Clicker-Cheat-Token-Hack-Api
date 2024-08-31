d140098a387552298f36a5c1dc8ca09b262d399d
LeeVee127 <160335475+LeeVee127@users.noreply.github.com>
2024-08-31 06:36:09 -0700

    BaseGame.cs

---
+++ BrilliantCrypto-Bot/MiniGames/BaseGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+using NosGame.MVVM;
+
+namespace NosGame.MiniGames;
+
+public abstract class BaseGame
+{
+    public abstract NosTaleWindow Window { get; set; }
+    public abstract int Points { get; set; }
+    protected abstract Timer? ReadPoints { get; set; }
+    public abstract void Update();
+
+    public void StopReadPoints()
+    {
+        if (ReadPoints is not {Enabled: true}) return;
+        
+        ReadPoints.Enabled = false;
+        ReadPoints.Stop();
+        ReadPoints.Dispose();
+        Points = 0;
+    }
+}
